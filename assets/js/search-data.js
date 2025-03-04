fdb65acd136b941ec99004a6b726450dc086e701
Innovai-yonsei <innovai.yonsei@gmail.com>
2025-03-04 02:36:35 +0000

    Deploying to gh-pages from @ Innovai-yonsei/InnovAI@bf2e3c64ec77197c35f51216ce112e643a26a68f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,28 +7,28 @@ ninja.data = [{
     title: "About",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/";
+      window.location.href = "/InnovAI/";
     },
   },{id: "nav-notice",
           title: "Notice",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/notice/";
+            window.location.href = "/InnovAI/notice/";
           },
         },{id: "nav-seminar",
           title: "Seminar",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/seminar/index.html";
+            window.location.href = "/InnovAI/seminar/index.html";
           },
         },{id: "nav-photos",
           title: "Photos",
           description: "Sharing photos from various events and seminars.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/photos/";
+            window.location.href = "/InnovAI/photos/";
           },
         },{id: "post-flame-model",
       
@@ -38,7 +38,7 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/FLAME_Model/";
+          window.location.href = "/InnovAI/blog/2025/FLAME_Model/";
         
       },
     },{id: "post-lrm",
@@ -49,69 +49,69 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/LRM/";
+          window.location.href = "/InnovAI/blog/2025/LRM/";
         
       },
     },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/InnovAI/books/the_godfather/";
             },},{id: "news-innovai-a-new-beginning",
           title: 'InnovAI: A New Beginning!',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_1/";
+              window.location.href = "/InnovAI/news/announcement_1/";
             },},{id: "news-innovai-homepage-is-now-live",
           title: 'InnovAI Homepage is Now Live! 🚀',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/InnovAI/news/announcement_2/";
             },},{id: "projects-kickoff-meeting",
           title: 'Kickoff Meeting',
           description: "2025/02/17 (Mon) 14:00 D403",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/InnovAI/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/InnovAI/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/InnovAI/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
+              window.location.href = "/InnovAI/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/InnovAI/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
+              window.location.href = "/InnovAI/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/InnovAI/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/InnovAI/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/InnovAI/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -131,7 +131,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("/InnovAI//feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
