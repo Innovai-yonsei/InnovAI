874ccc09027806823792b53cc3eceeebb8397096
Innovai-yonsei <innovai.yonsei@gmail.com>
2025-03-13 11:46:23 +0000

    Deploying to gh-pages from @ Innovai-yonsei/innovai-yonsei.github.io@a004c394b81153b2383ecee512aa7ab39c2b0ea6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -104,9 +104,9 @@ ninja.data = [{
           description: "2025/03/07 (Fri) 15:00 D404",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-march-week-2-seminar",
+          title: 'March : Week 2 Seminar',
+          description: "2025/03/13 (Thu) 19:00 D407",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
